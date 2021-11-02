42e0f69aacab95fc911772204bd1b2a9e5c4be13
Maqueda Paul <maquedapablodavid@gmail.com>
2021-11-01 23:01:06 -0300

    Subo entrega TP3

--- TP3/main.c
+++ TP3/main.c
@@ -2,5 +2,37 @@
 Apellido Nombres - Legajo
 
 Maqueda Pablo David - 1762620
+Bruno Juan Pablo    - 1768920
+GonzalezRiverol Camila - 174566
 
+3
 */
+
+#include "scanner.h"
+int TOKEN;
+char *token_names[] = {"Fin de archivo", "Programa", "Leer", "Entero", "Escribir", "Asignacion", "Identificador", "Constante"};
+
+int main()
+{
+    do
+    {
+        TOKEN = yylex();
+        if (TOKEN <= ASIGNACION)
+        {
+            // Muestro el TOKEN correspondiente a la gramatica
+            printf("Token: %s\n", token_names[TOKEN]);
+        }
+        else if (TOKEN == IDENTIFICADOR || TOKEN == CONSTANTE)
+        {
+            // Ademas del TOKEN , aca muestro el lexema que corresponde
+            printf("Token: %s\tlexema: %s\n", token_names[TOKEN], yytext);
+        }
+        else
+        {
+            // Si es solo un caracter el TOKEN, muestro ese caracter
+            printf("Token: \'%c\'\n", TOKEN);
+        }
+    } while (TOKEN != FDT);
+
+    return EXIT_SUCCESS;
+}
