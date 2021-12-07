862dabf8949caf387bd2a10817072a755a360552
Maqueda Paul <maquedapablodavid@gmail.com>
2021-12-06 23:13:23 -0300

    Subo TP 4

---
+++ TP4/main.c
@@ -0,0 +1,30 @@
+/*Datos del grupo
+Apellido Nombres - Legajo
+
+Maqueda Pablo David - 1762620
+Bruno Juan Pablo    - 1768920
+Gonzalez Riverol Camila - 1774566
+
+*/
+
+#include <stdio.h>
+#include "parser.h"
+
+int main(void)
+{
+    switch (yyparse())
+    {
+    case 0:
+        printf("\nCompilacion terminada con exito");
+        break;
+    case 1:
+        printf("\nErrores de compilacion");
+        break;
+    case 2:
+        printf("\nNo hay memoria suficiente");
+        break;
+    }
+    printf("\nErrores sintacticos:  %d - Errores lexicos:  %d\n", yynerrs, erroreslexicos);
+
+    return 0;
+}
